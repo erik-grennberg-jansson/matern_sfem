e09968fa9bc09361f624dd69e4b87fe4185318b0
erik-grennberg-jansson <jansson.grennberg.erik@gmail.com>
2021-08-10 11:04:33 +0200

    Update example.py

--- example.py
+++ example.py
@@ -1,33 +1,33 @@
-from numpy.random import normal
+#necessary imports 
+from numpy.random import normal 
 import numpy as np
 from dolfin import *
 from mesh_generation import sphere_mesh
 from utils import solve_problem
-#from Problem import Problem
-from field_sfem import problem_const
+from field_sfem import problem_const 
 
 
 
-L = 100
-beta = 0.51
-kappa = 1.0
-k = 2
-h = 0.01
+L = 100 #number of terms in KL-expansion
+beta = 0.51 #smoothness parameter
+kappa = 1.0 #length scale parameter
+k = 2 #quadrature step size 
+h = 0.01 # approx. mesh size 
 
-Problem = problem_const(L,beta,kappa,k,h)
+Problem = problem_const(L,beta,kappa,k,h) #create Problem
+ 
+Problem.set_rand(normal(0,1,[L+1,L+1,2]).flatten()) #set random numbers 
 
-Problem.set_rand(normal(0,1,[L+1,L+1,2]).flatten())
-print(Problem)
-surface = sphere_mesh(h)
-V= FunctionSpace(surface,FiniteElement("Lagrange",surface.ufl_cell(),1))
+surface = sphere_mesh(h) #create surface 
+V= FunctionSpace(surface,FiniteElement("Lagrange",surface.ufl_cell(),1)) #FEniCS function space on surface. 
 
 
-sol = Function(V)
+sol = Function(V) #placeholder for solution. 
 print("solving")
-#sol.vector()[:] = Problem.solve_sequentially() #SEQ SOLVE IS SLOW! 
-sol.vector()[:] = solve_problem(Problem,5)
+sol.vector()[:] = Problem.solve_sequentially() #solve sequentially
+
 print("solved")
 
 print("saving")
-file = File('field.pvd')
-file << sol
+file = File('field.pvd') #save for plotting in external software
+file << sol 
